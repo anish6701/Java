44164e34459869ab3da30b77b7cac648b953b2f8
Anish <60069224+anish6701@users.noreply.github.com>
2022-01-27 12:53:39 +0530

    Finding the area of a circle

---
+++ Area.java
@@ -0,0 +1,12 @@
+package demo1;
+
+public class Area {
+
+	public static void main(String[] args) {
+		int r = 10;
+		double pi = 3.14;
+		double a = pi * r * r;
+		System.out.println("Area of a cirlce is " + a + " sqcm");
+	}
+
+}
