cde6b7591306f8e251d79b605ebcbd23969b1e09
Anish <60069224+anish6701@users.noreply.github.com>
2022-01-27 12:25:48 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,18 @@
+package demo1;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 10;
+		int c = a + b;
+		int d = a - b;
+		int e = a * b;
+		int f = a / b;
+		System.out.println("Addition is :" + c);
+		System.out.println("Subtraction  is : " + d);
+		System.out.println("Multiplication is : " + e);
+		System.out.println("division is : " + f);
+	}
+
+}
